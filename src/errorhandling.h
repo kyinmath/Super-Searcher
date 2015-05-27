ef78e0975d4ec92dbea8d094bbe29a0ee360bf0b
Kurona <iusellvm@gmail.com>
2015-05-27 14:55:11 -0700

    docs

---
+++ src/errorhandling.h
@@ -0,0 +1,10 @@
+#include <string>
+#include "cs11.h"
+using std::string;
+[[noreturn]] inline void error(const string& Str) { outstream << "Error: " << Str << '\n'; abort(); 
+}
+
+//the condition is true when the program behaves normally.
+inline void check(bool condition, const string& Str) { if (!condition) error(Str); }
+
+inline void debugcheck(bool condition, const string& Str) { if (!condition) error(Str); }
