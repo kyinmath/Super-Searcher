8ca6e2647c822ebd240d3707b4fab9e311c99f9e
Kurona <iusellvm@gmail.com>
2015-08-02 17:47:47 -0700

    stream snapshot try. doesn't work because we need random access; we allocate, but write later.

--- src/serialization_snapshot.cpp
+++ src/serialization_snapshot.cpp
@@ -2,11 +2,9 @@
 #include <fstream>
 #include <iostream>
 
-void make_sample_file()
+void make_sample_file(uint64_t id)
 {
-	std::ofstream sample_file("awesomeness", std::ios::binary);
-
-	std::cout << sizeof(uint64_t) << '\n';
+	std::ofstream sample_file(to_string(id), std::ios::binary);
 	uint64_t integer_list[] = {46, 525336, 351523, 582357};
 
 	char* char_interp = reinterpret_cast<char*>(integer_list);
@@ -17,8 +15,8 @@ void make_sample_file()
 
 int main()
 {
-	make_sample_file();
-	std::ifstream sample_file("awesomeness", std::ios::binary | std::ios::ate);
+	make_sample_file(400);
+	std::ifstream sample_file(to_string(400), std::ios::binary | std::ios::ate);
 	unsigned long long size_of_file = sample_file.tellg();
 	unsigned long long no_of_integers = size_of_file >> 3; //uint64_t is 2^3 bytes
 	check(8 * no_of_integers == size_of_file, "File doesn't consist of uint64_t (number of bytes not a multiple of 8)");
@@ -34,6 +32,19 @@ int main()
 		std::cout << int_interp[output_incrementor] << ' ';
 	}
 	//std::cin.get();
+}
+
+
+void serialize(uint64_t id)
+{
+	std::ofstream sample_file(to_string(id), std::ios::binary);
+	
+	uint64_t header[] = {123, 345, 666666, 949494}; //we need a header to get past Typen("never reached")
+
+	char* char_interp = reinterpret_cast<char*>(integer_list);
+
+	sample_file.write(char_interp, sizeof(integer_list));
+	sample_file.close();
 }
 
 //returns an offset number.
